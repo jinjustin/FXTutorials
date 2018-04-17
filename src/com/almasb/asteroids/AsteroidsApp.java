57394997eb0f540adb0ec2dc70cd0d3f37ed48e2
jinjustin <32735269+jinjustin@users.noreply.github.com>
2018-04-17 12:23:48 +0700

    Update AsteroidsApp.java

--- src/com/almasb/asteroids/AsteroidsApp.java
+++ src/com/almasb/asteroids/AsteroidsApp.java
@@ -14,27 +14,53 @@ import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.List;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
-/**
- * @author Almas Baimagambetov (almaslvl@gmail.com)
- */
-public class AsteroidsApp extends Application {
+public class Astroid extends Application { //เขียน JavaFx ต้อง extends Application เสมอ
 
-    private Pane root;
+    private Pane root; //หน้าจอเกม
+    
+    private StackPane menuRoot; //หน้าจอ Menu ตอนเข้าเกม
 
-    private List<GameObject> bullets = new ArrayList<>();
-    private List<GameObject> enemies = new ArrayList<>();
+    private List<GameObject> bullets = new ArrayList<>(); //List ของ GameObject ใช้เป็นกระสุน
+    private List<GameObject> enemies = new ArrayList<>(); //List ของ GameObject ใช้เป็นศัตรู
+    private List<GameObject> tanks = new ArrayList<>();
 
+    private int score=0,power=10,HP=10;
+    private int powerCount=0;
+    
     private GameObject player;
+    private GameObject hpBar;
+    private GameObject powerBar;
+    
+    private Text scoreText = new Text("Score : "+ score); //Text ของคะแนน
 
-    private Parent createContent() {
-        root = new Pane();
-        root.setPrefSize(600, 600);
-
+    private Parent gameContent() { //ยังงงว่า Parent คืออะไร
+        root = new Pane(); //กำหนดให้ root เป็น layout อันใหม่
+        root.setPrefSize(1300, 1000);
+        
         player = new Player();
-        player.setVelocity(new Point2D(1, 0));
-        addGameObject(player, 300, 300);
+        
+        hpBar = new HPbar();
+        powerBar = new PowerBar();
+        
+        player.setVelocity(new Point2D(2, 0)); //ความเร็วเริ่มต้นพอกดเปลี่ยนทิศทางความเร็วเหลือ 1   
+        addGameObject(player, 300, 300); //Method อยู่ในคลาสนี้
 
+        
+        scoreText.setFont(Font.font("Sans serif",FontWeight.NORMAL,FontPosture.REGULAR,50));
+        scoreText.setFill(Color.RED);
+        scoreText.setX(1030.0);
+        scoreText.setY(940.0);
+        root.getChildren().add(scoreText);
+        
+        addGameObject(hpBar,20,900);
+        addGameObject(powerBar,20,940);
+        
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
@@ -42,12 +68,21 @@ public class AsteroidsApp extends Application {
             }
         };
         timer.start();
-
         return root;
+    }
+    
+    private Parent gameMenu(){ //incompleted
+        
+        return menuRoot;
+    }
+    
+    private int randomWithRange(int min, int max){ //สุ่มตัวเลข
+   int range = (max - min) + 1;     
+   return (int)(Math.random() * range) + min;
     }
 
     private void addBullet(GameObject bullet, double x, double y) {
-        bullets.add(bullet);
+        bullets.add(bullet); //เพิ่ม Object ลงใน ArrayList
         addGameObject(bullet, x, y);
     }
 
@@ -55,36 +90,106 @@ public class AsteroidsApp extends Application {
         enemies.add(enemy);
         addGameObject(enemy, x, y);
     }
+    
+    private void addTank(GameObject tank, double x, double y) {
+        tanks.add(tank);
+        addGameObject(tank, x, y);
+    }
 
     private void addGameObject(GameObject object, double x, double y) {
         object.getView().setTranslateX(x);
         object.getView().setTranslateY(y);
-        root.getChildren().add(object.getView());
+        root.getChildren().add(object.getView()); // ไม่รู้ว่า children คืออะไร 
     }
 
     private void onUpdate() {
+        
         for (GameObject bullet : bullets) {
             for (GameObject enemy : enemies) {
                 if (bullet.isColliding(enemy)) {
                     bullet.setAlive(false);
                     enemy.setAlive(false);
-
+                    score+=1;
+                    System.out.println("Score : "+score);
                     root.getChildren().removeAll(bullet.getView(), enemy.getView());
+                    GameObject.enemiesCount--;
                 }
             }
+            if(bullet.getView().getTranslateY()>=880){
+                bullet.setAlive(false);
+                root.getChildren().removeAll(bullet.getView());
+            }
         }
+        
+            for (GameObject enemy : enemies) {
+                if (player.isColliding(enemy)) {
+                    HP-=1;
+                    System.out.println("HP : "+HP);
+                    enemy.setAlive(false);
+                    System.out.println("Score : "+score);
+                    root.getChildren().removeAll(enemy.getView());
+                    GameObject.enemiesCount--;
+                }
+            }
+        
+            for (GameObject tank : tanks) { 
+                if (player.isColliding(tank)) {
+                    tank.setAlive(false);
+                    if(HP<=5) HP+=5;
+                    else HP=10;
+                    System.out.println("HP : "+HP);
+                    root.getChildren().removeAll(tank.getView());
+                    GameObject.tanksCount--;
+                }
+            }
 
-        bullets.removeIf(GameObject::isDead);
+        bullets.removeIf(GameObject::isDead); //Method isDead อยู่ในคลาส GameObject
         enemies.removeIf(GameObject::isDead);
+        tanks.removeIf(GameObject::isDead);
 
-        bullets.forEach(GameObject::update);
+        bullets.forEach(GameObject::update); //update อยู่ในคลาส GameObject
         enemies.forEach(GameObject::update);
+        tanks.forEach(GameObject::update);
 
-        player.update();
+        player.playerUpdate();
 
-        if (Math.random() < 0.02) {
-            addEnemy(new Enemy(), Math.random() * root.getPrefWidth(), Math.random() * root.getPrefHeight());
+        if (randomWithRange(0,100) < 2 && GameObject.enemiesCount<20) {
+            addEnemy(new Enemy(), Math.random() * root.getPrefWidth(), Math.random() * 880.0);
+            GameObject.enemiesCount++;
         }
+        if (randomWithRange(0,3000) < 2 && GameObject.tanksCount<2) {
+            addTank(new Tank(), Math.random() * root.getPrefWidth(), Math.random() * 855.0);
+            GameObject.tanksCount++;
+        }
+        powerCount++;
+        if(player.getBoost()==false){
+        if(powerCount>=60 && power<10){
+            power+=1;
+            powerCount=0;
+        }
+        }
+        else{
+            if(powerCount>=30 && power>0){
+            power-=1;
+            powerCount=0;
+                System.out.println("power : "+power);
+        }
+            else if(powerCount>=30 && power<=0){
+               player.setBoost(false);
+            }
+        }
+        
+        //คะแนน
+        scoreText.setText("Score : " + score);
+        
+        root.getChildren().removeAll(hpBar.getView());
+        hpBar.hpUpdate(HP);
+        addGameObject(hpBar,20,900);
+        
+        root.getChildren().removeAll(powerBar.getView());
+        powerBar.powerUpdate(power);
+        addGameObject(powerBar,20,940);
+        
     }
 
     private static class Player extends GameObject {
@@ -104,22 +209,58 @@ public class AsteroidsApp extends Application {
             super(new Circle(5, 5, 5, Color.BROWN));
         }
     }
+    
+    private static class Tank extends GameObject {
+        Tank() {
+            super(new Rectangle(20, 40, Color.GREEN));
+        }
+    }
+    
+    private class HPbar extends GameObject {
+        HPbar(){
+            super(new Rectangle(100*HP,30, Color.RED));
+        }
+    }
+    
+    private class PowerBar extends GameObject {
+        PowerBar(){
+            super(new Rectangle(100*power,30, Color.YELLOW));
+        }
+    }
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setScene(new Scene(createContent()));
+        stage.setScene(new Scene(gameContent()));
         stage.getScene().setOnKeyPressed(e -> {
             if (e.getCode() == KeyCode.LEFT) {
                 player.rotateLeft();
             } else if (e.getCode() == KeyCode.RIGHT) {
                 player.rotateRight();
             } else if (e.getCode() == KeyCode.SPACE) {
+                if(power>0){
                 Bullet bullet = new Bullet();
+                if(!player.getBoost())
                 bullet.setVelocity(player.getVelocity().normalize().multiply(5));
+                else
+                    bullet.setVelocity(player.getVelocity().normalize().multiply(8));
                 addBullet(bullet, player.getView().getTranslateX(), player.getView().getTranslateY());
+                power-=1;
+                System.out.println("Power : " + power);
+                }
+            }
+            else if (e.getCode() == KeyCode.SHIFT){
+                player.setBoost(!player.getBoost());
+                if(player.getBoost())
+                    player.setVelocity(player.getVelocity().normalize().multiply(4));
+                else
+                    player.setVelocity(player.getVelocity().normalize().multiply(2));
             }
         });
         stage.show();
+    }
+    
+    public void menu(Stage stage) throws Exception{
+        
     }
 
     public static void main(String[] args) {
